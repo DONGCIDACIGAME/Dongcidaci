f840c0f4d11a83cc5388ee889d1fb0c107e5cf2e
samwengwu <42069182+samwengwu@users.noreply.github.com>
2023-05-13 21:28:27 +0800

    0513_2128_weng

--- Assets/Scripts/Collider/InterfaceGameCollider.cs
+++ Assets/Scripts/Collider/InterfaceGameCollider.cs
@@ -1,7 +1,20 @@
 using UnityEngine;
 
+public interface IGameCollider
+{
+    int GetColliderType();
+    void OnColliderEnter(IGameCollider other);
+}
+
+
 public interface IGameCollider2D : IGameCollider
 {
     bool CheckPosInCollider(Vector2 pos);
     bool CheckCollapse(Rect rect);
 }
+
+
+
+
+
+
