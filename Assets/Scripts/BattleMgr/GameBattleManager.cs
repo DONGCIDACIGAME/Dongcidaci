355156114f94119b393e0f97d14c080443ef18a8
stephenhou2 <stephenhou2@163.com>
2023-07-02 23:54:57 +0800

    修改笔误

--- Assets/Scripts/BattleMgr/GameBattleManager.cs
+++ Assets/Scripts/BattleMgr/GameBattleManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameBattleManager : ModuleManager<GameColliderManager>
+public class GameBattleManager : ModuleManager<GameBattleManager>
 {
 
 
