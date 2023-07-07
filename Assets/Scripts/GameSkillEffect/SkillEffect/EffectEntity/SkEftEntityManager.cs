1a940d01f235533152d8f63e4c799c02aac2a72c
samwengwu <qinwuweng@gmail.com>
2023-07-07 20:52:37 +0800

    增加SkEftEntityManager管理具有实体的技能效果

---
+++ Assets/Scripts/GameSkillEffect/SkillEffect/EffectEntity/SkEftEntityManager.cs
@@ -0,0 +1,39 @@
+using GameEngine;
+
+
+namespace GameSkillEffect
+{
+    public class SkEftEntityManager : ModuleManager<SkEftEntityManager>, IMeterHandler
+    {
+        public override void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            //base.OnUpdate(deltaTime);
+
+
+        }
+
+
+        public void OnMeterEnter(int meterIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnMeterEnd(int meterIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+}
+
--- Assets/Scripts/GameSkillEffect/SkillEffect/Effects/CastEffect/FireBullet.cs
+++ Assets/Scripts/GameSkillEffect/SkillEffect/Effects/CastEffect/FireBullet.cs
@@ -127,7 +127,6 @@ namespace GameSkillEffect
         public override void RecycleReset()
         {
             Dispose();
-
         }
 
         
